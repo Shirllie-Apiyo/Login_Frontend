e78f427888eb944304ff0a49824c2c4ee2723bb4
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-20 11:05:13 +0300

    component login & add component

---
+++ login/src/app/add-login/add-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-login',
+  templateUrl: './add-login.component.html',
+  styleUrls: ['./add-login.component.css']
+})
+export class AddLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ login/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddLoginComponent } from './add-login/add-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
