d112d1dee5bc3e5c850a77f9cb08de6f54674bcf
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-20 22:10:48 +0300

    import modules

--- login/src/app/add-login/add-login.component.ts
+++ login/src/app/add-login/add-login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-login',
   templateUrl: './add-login.component.html',
   styleUrls: ['./add-login.component.css']
 })
 export class AddLoginComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
--- login/src/app/app-routing.module.ts
+++ login/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddLoginComponent } from './add-login/add-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
