5150fb90944bce26e6e41f271df75e2822d770b5
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-21 10:38:24 +0300

    server errors

--- login/src/app/add-login/add-login.component.ts
+++ login/src/app/add-login/add-login.component.ts
@@ -7,8 +7,34 @@ import { FormBuilder,FormGroup,Validators } from '@angular/forms';
   styleUrls: ['./add-login.component.css']
 })
 export class AddLoginComponent implements OnInit {
- 
-  constructor() { }
+  form:FormGroup
+  constructor(
+    public fb:FormBuilder,
+    private http:HttpClient
+  ) { 
+    this.form = this.fb.group({
+      first_name:[''],
+      last_name:[''],
+      phone:[''],
+      email:[''],
+      message:[''],
+    });
+   }
+   successmessage:any
+   firstnameerror:any
+   lastnameerror:any
+   phoneerror:any
+   emailerror:any
+   messageerror:any
+    submitForm(){
+      this.http.post<any>('http://127.0.0.1:5000/api//add',{
+          "first_name":this.form.get("first_name")!.value,
+          "last_name":this.form.get("last_name")!.value,
+          "phone":this.form.get("phone")!.value,
+          "email":this.form.get("email")!.value,
+          "message":this.form.get("message")!.value
+      })
+    }
 
   ngOnInit(): void {
   }
